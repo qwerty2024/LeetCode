5bf7bffda7a453bbbb9b1d01774bf135943f6817
qwerty2024 <multanmgu@gmail.com>
2024-12-20 11:19:10 +0300

    326

---
+++ easy/326.cpp
@@ -0,0 +1,13 @@
+class Solution 
+{
+public:
+    bool isPowerOfThree(int n) 
+    {
+        if (n > 0)
+        {
+            return 1162261467 % n == 0;
+        }
+
+        return false;
+    }
+};
