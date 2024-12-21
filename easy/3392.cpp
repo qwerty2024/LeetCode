769400bf32baed070de302a49c56d9ca0b0a981c
qwerty2024 <multanmgu@gmail.com>
2024-12-21 19:13:32 +0300

    3392

---
+++ easy/3392.cpp
@@ -0,0 +1,19 @@
+class Solution 
+{
+public:
+    int countSubarrays(vector<int>& nums) 
+    {
+        int ans = 0;
+        int n = nums.size();
+        for (int i = 0; i < n - 2; i++)
+        {
+            if (nums[i + 1] % 2 == 0)
+                if (nums[i] + nums[i + 2] == nums[i + 1] / 2)
+                {
+                    ans++;
+                }
+        }
+
+        return ans;
+    }
+};
