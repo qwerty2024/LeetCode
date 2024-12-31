fd42116fabd64d7c1f5f7296c3f0f44910e65050
qwerty2024 <multanmgu@gmail.com>
2024-12-31 23:11:12 +0300

    2016

---
+++ easy/2016.cpp
@@ -0,0 +1,21 @@
+
+class Solution 
+{
+public:
+    int maximumDifference(vector<int>& nums) 
+    {
+        int maxi = -1;
+        int n = nums.size();
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[i] < nums[j])
+                    maxi = max(nums[j] - nums[i], maxi);
+            }
+        }
+
+        return maxi;
+    }
+};
