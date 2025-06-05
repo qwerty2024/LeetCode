75c2cfd5c05ae0f65a9458bf762a465c51d5b480
qwerty2024 <multanmgu@gmail.com>
2025-06-05 20:21:09 +0300

    153

---
+++ mideum/153.cpp
@@ -0,0 +1,26 @@
+class Solution 
+{
+public:
+    int findMin(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int l = 0;
+        int r = n - 1;
+
+        while (l < r)
+        {
+            int mid = (r + l) / 2;
+
+            if (nums[mid] < nums[r])
+            {
+                r = mid;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+
+        return nums[l];
+    }
+};
