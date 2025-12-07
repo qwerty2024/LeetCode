304aed11eaba89d759e4d96e22b66670accc34ec
qwerty2024 <multanmgu@gmail.com>
2025-12-07 15:54:16 +0300

    1523

---
+++ easy/1523.cpp
@@ -0,0 +1,15 @@
+class Solution 
+{
+public:
+    int countOdds(int low, int high) 
+    {
+        if (low % 2 == 0)
+        {
+            return (high - low + 1) / 2;
+        }
+        else
+        {
+            return (high - low) / 2 + 1;
+        }
+    }
+};
