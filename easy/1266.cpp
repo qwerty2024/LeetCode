4c43f6883034fbdb3df2c516fefe2a8be631e19e
qwerty2024 <multanmgu@gmail.com>
2026-01-12 06:10:56 +0300

    1266

---
+++ easy/1266.cpp
@@ -0,0 +1,18 @@
+class Solution 
+{
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) 
+    {
+        int ans = 0;
+
+        for (int i = 1; i < points.size(); i++)
+        {
+            int dx = abs(points[i - 1][0] - points[i][0]);
+            int dy = abs(points[i - 1][1] - points[i][1]);
+
+            ans += min(dx, dy) + max(dx, dy) - min(dx, dy);
+        }
+
+        return ans;
+    }
+};
