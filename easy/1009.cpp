7302f056b0a0da2c939ceb7f893d4e7213284776
qwerty2024 <multanmgu@gmail.com>
2026-03-11 21:27:43 +0300

    1009

---
+++ easy/1009.cpp
@@ -0,0 +1,23 @@
+class Solution 
+{
+public:
+    int bitwiseComplement(int n) 
+    {
+        int ans = 0;
+
+        if (n == 0)
+            return 1;
+
+        long long step = 1;
+        while (n > 0)
+        {
+            if (!(n & 1))
+                ans += step;
+
+            step <<= 1;
+            n >>= 1;
+        }
+
+        return ans;
+    }
+};
